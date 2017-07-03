802b38426301aa533c615f0646d93b51f5e0dc2e
Lamprini Vasilaki <lamprinivasilaki@gmail.com>
2017-07-03 09:47:00 +0300

    initial commit

---
+++ src/components/header.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from '../images/logo.svg';
+import '../styles/App.css';
+
+
+export default class Header extends Component {
+  render() {
+    return (
+		<div className="App-header">
+			<img src={logo} className="App-logo" alt="logo" />
+			<h2>Welcome to Review Finder</h2>
+
+			<p className="App-intro">
+				Find reviews from your favorite music songs and albums on Review Finder.
+	        </p>
+		</div>
+	);
+  }
+}
---
+++ src/getSignature.js
@@ -0,0 +1,8 @@
+// function genSig() {
+//     var apikey = "valid_apikey";
+//     var secret = "valid_secret";
+//     var curdate = new Date();
+//     var gmtstring = curdate.toGMTString();
+//     var utc = Date.parse(gmtstring) / 1000;
+//     return = hex_md5(apikey + secret + utc);
+// }
